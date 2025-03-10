a84d198f42ca886d4c21ac3090dc95458a74b7e3
Mohamed Fathy <mofathy1833@gmail.com>
2025-03-10 15:45:37 +0200

    delete unnecssary comments and fils

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,11 @@ export default {
 	setupFilesAfterEnv: ['./src/tests/setup.test.js'],
 	testTimeout: 3000,
 	testMatch: [
-		// "**/src/tests/**/bag.test.js",
-		// "**/src/tests/**/suitcase.test.js",
-		// "**/src/tests/**/item.test.js",
-		// "**/src/tests/**/user.test.js",
-		// "**/src/tests/**/auth.test.js",
-		'**/src/tests/**/setup.test.js',
+		"**/src/tests/**/bag.test.js",
+		"**/src/tests/**/suitcase.test.js",
+		"**/src/tests/**/item.test.js",
+		"**/src/tests/**/user.test.js",
+		"**/src/tests/**/auth.test.js",
 	], // Ensure Jest looks inside `src/tests`
 	testPathIgnorePatterns: ['/node_modules/'],
 	forceExit: true,
